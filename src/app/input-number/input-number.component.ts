818d6e04e8675dc7510353ed482288f6ebcdf74c
BrendaLeveni1 <brendaleveni4@gmail.com>
2024-07-03 21:53:14 -0300

    acomodo estilos

--- src/app/input-number/input-number.component.ts
+++ src/app/input-number/input-number.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Velas } from '../velas-list/Velas';
 
 @Component({
@@ -10,43 +10,48 @@ export class InputNumberComponent implements OnInit{
 
   
   constructor(){}
-  @Input()
-  velas!: Velas;
-
+  @Input() quantity !: number;
+  @Input() max !: number;
+  @Output() quantityChange : EventEmitter<number> =new EventEmitter<number>();
+    
   
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
-  upQuantity(velas : Velas) {
-    if(velas.quantity <velas.stock){
+  upQuantity() {
+    if(this.quantity <this.max){
   
-      velas.quantity++;
+      this.quantity++;
+      this.quantityChange.emit(this.quantity);
+    }
+    
+  }
+  dowQuantity() {
+    if(this.quantity >0){
+  
+      this.quantity--;
+      this.quantityChange.emit(this.quantity);
     }
   }
-  dowQuantity(velas : Velas) {
-    if(velas.quantity >0){
   
-      velas.quantity--;
-    }
-  }
-  
-  onChangeQuantity(event: any, velas: Velas): void {
+  onChangeQuantity(event: any): void {
     const input = event.target as HTMLInputElement;
     let value = parseInt(input.value, 10);
   
     // Prevent non-numeric input
     if (isNaN(value)) {
-      input.value = velas.quantity.toString();
+      input.value = this.quantity.toString();
       return;
     }
   
     // Prevent exceeding stock
-    if (value > velas.stock) {
-      input.value = velas.stock.toString();
-      value = velas.stock;
+    if (value >this.max) {
+      input.value = this.max.toString();
+      value = this.max;
     }
   
     // Update quantity
-    velas.quantity = value;
+    this.quantity = value;
+    this.quantityChange.emit(this.quantity);
   }
 }
